4da118ef8256d6e329e4d25ea5664a3e0cff6e87
BrezhnevAleks <aleksbrezhnev@gmail.com>
2020-10-08 18:03:45 +0300

    Created styles

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ class TodoApp extends React.Component {
       text: "",
       allin: true,
       elementDisplay: true,
+      showSwitch: 1,
     };
   }
 
@@ -13,8 +14,18 @@ class TodoApp extends React.Component {
     return (
       <div className="container">
         <h1>todos</h1>
-        <div className="choose-all" onClick={this.handleChangeAll}></div>
+
         <form onSubmit={this.handleSubmit}>
+          <div
+            className="choose-all"
+            onClick={this.handleChangeAll}
+            style={
+              this.state.items.length ? { opacity: "1" } : { opacity: "0" }
+            }
+            style={this.state.allin ? { color: "" } : { color: "#737373" }}
+          >
+            <span className="marker">❯</span>
+          </div>
           <input
             placeholder="What needs to be done?"
             id="new-todo"
@@ -32,10 +43,10 @@ class TodoApp extends React.Component {
           handleNewSubmit={this.handleNewSubmit}
           handleNewText={this.handleNewText}
           handleNewChange={this.handleNewChange}
-          handleNewBack={this.handleNewBack}
           handleNewTextDeactive={this.handleNewTextDeactive}
         />
         <Footer
+          showSwitch={this.state.showSwitch}
           items={this.state.items}
           handleShow={this.handleShow}
           handleComplited={this.handleComplited}
@@ -65,21 +76,13 @@ class TodoApp extends React.Component {
       items,
     }));
   };
-  handleNewBack = (e) => {
-    e.preventDefault();
-    if (e.keyCode === 27) {
-      let { items } = this.state;
-
-      items.forEach((item) => (item.formDisplay = false));
-      this.setState((state) => ({ items }));
-    }
-  };
 
   handleNewText = (e, index) => {
     e.preventDefault();
 
     let { items } = this.state;
-
+    items.forEach((item) => (item.formDisplay = false));
+    this.setState((state) => ({ items }));
     items[index].formDisplay = !items[index].formDisplay;
     this.setState((state) => ({ items }));
   };
@@ -87,7 +90,11 @@ class TodoApp extends React.Component {
   handleNewTextDeactive = (e) => {
     e.preventDefault();
     let { items } = this.state;
-    items.forEach((item) => (item.formDisplay = false));
+    items.forEach((item) =>
+      item.formDisplay === true
+        ? (item.formDisplay = false)
+        : (item.formDisplay = item.formDisplay)
+    );
     this.setState((state) => ({ items }));
   };
 
@@ -100,7 +107,7 @@ class TodoApp extends React.Component {
       text: this.state.text,
       id: Date.now(),
       flag: true,
-      display: "block",
+      display: "flex",
       formDisplay: false,
       formText: this.state.text,
     };
@@ -112,11 +119,17 @@ class TodoApp extends React.Component {
 
   handleFlag = (e, index) => {
     const { items } = this.state;
-
+    let { allin } = this.state;
     items[index].flag = !items[index].flag;
     this.setState((state) => ({
       items,
     }));
+    if (!this.state.items.filter((item) => item.flag).length) {
+      allin = false;
+    } else {
+      allin = true;
+    }
+    this.setState((state) => ({ allin }));
   };
 
   handleChangeAll = (e) => {
@@ -140,13 +153,22 @@ class TodoApp extends React.Component {
     items.forEach((item) => {
       switch (key) {
         case 1:
-          item.display = "block";
+          item.display = "flex";
+          this.setState((state) => ({
+            showSwitch: 1,
+          }));
           break;
         case 2:
-          item.display = item.flag ? "block" : "none";
+          item.display = item.flag ? "flex" : "none";
+          this.setState((state) => ({
+            showSwitch: 2,
+          }));
           break;
         case 3:
-          item.display = item.flag ? "none" : "block";
+          item.display = item.flag ? "none" : "flex";
+          this.setState((state) => ({
+            showSwitch: 3,
+          }));
           break;
       }
       this.setState((state) => ({ items }));
@@ -194,7 +216,6 @@ class TodoList extends React.Component {
             handleNewText={this.props.handleNewText}
             handleNewChange={this.props.handleNewChange}
             handleNewSubmit={this.props.handleNewSubmit}
-            handleNewBack={this.props.handleNewBack}
             handleNewTextDeactive={this.props.handleNewTextDeactive}
           />
         ))}
@@ -217,18 +238,19 @@ class LiItem extends React.Component {
         className={
           !!this.props.items[this.props.index].flag ? "active" : "complited"
         }
-        onDoubleClick={(e) => this.props.handleNewText(e, this.props.index)}
       >
+        <input
+          className="checkbox"
+          type="checkbox"
+          checked={!this.props.items[this.props.index].flag}
+          onChange={(e) => this.props.flagChange(e, this.props.index)}
+        />
         <div
           className={"item"}
+          onDoubleClick={(e) => this.props.handleNewText(e, this.props.index)}
           onClick={(e) => this.props.handleNewTextDeactive(e)}
         >
           <form>
-            <input
-              type="checkbox"
-              checked={!this.props.items[this.props.index].flag}
-              onChange={(e) => this.props.flagChange(e, this.props.index)}
-            />
             <label className={"text"}>{this.props.text}</label>
           </form>
           <button
@@ -236,7 +258,12 @@ class LiItem extends React.Component {
             className="destroy"
           ></button>
         </div>
-        <form onSubmit={(e) => this.props.handleNewSubmit(e, this.props.index)}>
+        <form
+          onSubmit={(e) => this.props.handleNewSubmit(e, this.props.index)}
+          onKeyDown={(e) => {
+            if (e.keyCode === 27) this.props.handleNewText(e, this.props.index);
+          }}
+        >
           <input
             onSubmit={(e) => this.props.handleNewText(e, this.props.index)}
             onChange={(e) => this.props.handleNewChange(e, this.props.index)}
@@ -265,19 +292,21 @@ class Footer extends React.Component {
     return (
       <div
         style={
-          this.props.items.length ? { display: "block" } : { display: "none" }
+          this.props.items.length ? { display: "flex" } : { display: "none" }
         }
+        className="footer"
       >
-        <span>
+        <button>
           {this.props.items.filter((item) => item.flag).length}
           {" item"}
           {this.props.items.filter((item) => item.flag).length === 1
             ? " left"
             : "s left"}
-        </span>
+        </button>
         <FooterMenu
           items={this.props.items}
           handleShow={this.props.handleShow}
+          showSwitch={this.props.showSwitch}
         />
         <ClearButton
           items={this.props.items}
@@ -291,10 +320,25 @@ class Footer extends React.Component {
 class FooterMenu extends React.Component {
   render() {
     return (
-      <ul>
-        <button onClick={(e) => this.props.handleShow(e, 1)}>All</button>
-        <button onClick={(e) => this.props.handleShow(e, 2)}>Active</button>
-        <button onClick={(e) => this.props.handleShow(e, 3)}>Complited</button>
+      <ul className="footer-menu">
+        <button
+          className={this.props.showSwitch === 1 ? "active-button" : ""}
+          onClick={(e) => this.props.handleShow(e, 1)}
+        >
+          All
+        </button>
+        <button
+          className={this.props.showSwitch === 2 ? "active-button" : ""}
+          onClick={(e) => this.props.handleShow(e, 2)}
+        >
+          Active
+        </button>
+        <button
+          className={this.props.showSwitch === 3 ? "active-button" : ""}
+          onClick={(e) => this.props.handleShow(e, 3)}
+        >
+          Complited
+        </button>
       </ul>
     );
   }
@@ -306,8 +350,8 @@ class ClearButton extends React.Component {
       <button
         style={
           this.props.items.filter((item) => !item.flag).length
-            ? { display: "block" }
-            : { display: "none" }
+            ? { opacity: "1" }
+            : { opacity: "0" }
         }
         onClick={(e) => this.props.handleComplited(e)}
       >
